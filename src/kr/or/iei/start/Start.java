3820be6e1c7106dfe18e04d28b6e4ea607718c6e
UnEmotioneD <blackeagle10@icloud.com>
2025-03-01 01:57:25 +0900

    Chore(start) : remove white space from Start.java

--- src/kr/or/iei/start/Start.java
+++ src/kr/or/iei/start/Start.java
@@ -3,7 +3,6 @@ package kr.or.iei.start;
 import kr.or.iei.controller.FridgeController;
 
 public class Start {
-
     public static void main(String[] args) {
         FridgeController fc = new FridgeController();
         fc.mainMethod();
